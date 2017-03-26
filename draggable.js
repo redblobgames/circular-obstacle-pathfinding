4a2e5ca2bdab3ec957006510a4aa373402722e8f
Amit Patel <redblobgames@gmail.com>
2017-03-26 10:39:52 -0700

    Add touch and multitouch event handling
    - Each finger is tracked separately, so you can drag multiple circles
      in a diagram at the same time

--- draggable.js
+++ draggable.js
@@ -6,10 +6,12 @@
     element: may move, gets mousedown handler
     handler: function called with (start, current, last)
     where last is the value returned last time handler called
-    TODO: handle touch events -- http://devdocs.io/dom/touch_events/supporting_both_touchevent_and_mouseevent
 */
 function makeDraggable(reference, element, handler) {
     element.addEventListener('mousedown', mouseDown);
+    element.addEventListener('touchstart', touchEvent);
+    element.addEventListener('touchmove', touchEvent);
+    element.addEventListener('touchend', touchEvent);
     
     function coords(rect, e) {
         return {x: e.clientX - rect.left, y: e.clientY - rect.top};
@@ -24,16 +26,40 @@ function makeDraggable(reference, element, handler) {
         function mouseMove(e) {
             last = handler(start, coords(rect, e), last);
             e.preventDefault();
+            e.stopPropagation();
         }
 
         function mouseUp(e) {
             window.removeEventListener('mousemove', mouseMove);
             window.removeEventListener('mouseup', mouseUp);
             e.preventDefault();
+            e.stopPropagation();
         }
 
         window.addEventListener('mousemove', mouseMove);
         window.addEventListener('mouseup', mouseUp);
         e.preventDefault();
+        e.stopPropagation();
+    }
+
+    let touch_start = [];
+    let touch_last = [];
+    
+    function touchEvent(e) {
+        for (let i = 0; i < e.changedTouches.length; i++) {
+            const rect = reference.getBoundingClientRect();
+            const touch = e.changedTouches[i];
+            let current = coords(rect, touch);
+            if (e.type == 'touchstart') {
+                touch_start[touch.identifier] = current;
+                touch_last[touch.identifier] = handler(current, current, null);
+            } else {
+                let start = touch_start[touch.identifier];
+                let last = touch_last[touch.identifier];
+                touch_last[touch.identifier] = handler(start, current, last);
+            }
+        }
+        e.preventDefault();
+        e.stopPropagation();
     }
 }
