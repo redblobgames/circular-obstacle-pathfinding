4da744cba9330e794ca885aeb24e638302eb6859
Amit Patel <redblobgames@gmail.com>
2017-04-04 02:17:55 -0700

    Add pathfinding diagram (incomplete), factor code
    - For the pathfinding diagram we'll need to calculate the hugging and
      surfing edges and then use them for the neighbors() function
    - Moved calculation of surfing edges out of diagram object and into functions
    - Added a pathfinding problem (but not yet pathfinding itself)

--- graph-diagrams.js
+++ graph-diagrams.js
@@ -2,49 +2,80 @@
 // Copyright 2017 Red Blob Games <redblobgames@gmail.com>
 // License: Apache v2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
 
+/** Check line of sight between circle i point P and circle j point Q
+    (excludes circles i,j from the test) 
+ */
+function line_of_sight(circles, i, P, j, Q) {
+    for (let k = 0; k < circles.length; k++) {
+        if (k != i && k != j
+            && segment_circle_intersection(P, Q, circles[k]).intersects) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+/** Generate surfing edges, [{circle: int, x: number, y: number},
+                             {circle: int, x: number, y: number}]
+ */
+function generate_surfing_edges(circles) {
+    let edges = [];
+
+    // try to add edge from circle i point P to circle j point Q
+    function add_edge(i, P, j, Q) {
+        if (isNaN(P.x) || isNaN(Q.x)) { return; }
+        if (!line_of_sight(circles, i, P, j, Q)) { return; }
+        edges.push([{circle: i, x: P.x, y: P.y},
+                    {circle: j, x: Q.x, y: Q.y}]);
+    }
+    
+    for (let i = 0; i < circles.length; i++) {
+        for (let j = 0; j < i; j++) {
+            let candidates = [];
+            var internal = new InternalBitangents(circles[i], circles[j]);
+            add_edge(i, internal.C, j, internal.F);
+            add_edge(i, internal.D, j, internal.E);
+            var external = new ExternalBitangents(circles[i], circles[j]);
+            add_edge(i, external.C, j, external.F);
+            add_edge(i, external.D, j, external.E);
+            // TODO: handle circles[i].r == 0 or circles[j].r == 0
+        }
+    }
+    return edges;
+}
+
+
 let graph_all_edges = new Vue({
     el: "#graph-all-edges",
     data: {
-        circles: [
-            {x:  80, y: 200, r: 70},
+        circles: [ // sorted by r
             {x: 340, y: 200, r: 90},
+            {x:  80, y: 200, r: 70},
             {x: 505, y:  65, r: 50}
         ]
     },
     computed: {
-        edges: function() {
-            let edges = [];
-            const circles = this.circles;
-            for (let i = 0; i < circles.length; i++) {
-                for (let j = 0; j < i; j++) {
-                    let candidates = [];
-                    var internal = new InternalBitangents(circles[i], circles[j]);
-                    candidates.push([internal.C, internal.F],
-                                    [internal.D, internal.E]);
-                    var external = new ExternalBitangents(circles[i], circles[j]);
-                    candidates.push([external.C, external.F],
-                                    [external.D, external.E]);
-                    edges.push.apply(edges,
-                                     candidates
-                                     .filter((e) => !isNaN(e[0].x) && !isNaN(e[1].x))
-                                     .filter((e) => this.line_of_sight(e[0], e[1], i, j)));
-                }
-            }
-            return edges;
-        }
+        edges: function() { return generate_surfing_edges(this.circles); }
+    }
+});
+
+
+let graph_busy_edges = new Vue({
+    el: "#graph-busy-edges",
+    data: {
+        circles: [ // sorted by r
+            {x: 180, y: 100, r: 55},
+            {x: 240, y: 230, r: 30},
+            {x: 340, y: 200, r: 30},
+            {x: 505, y:  65, r: 25},
+            {x: 405, y: 255, r: 20},
+            {x:  80, y: 200, r: 20},
+            {x:  20, y:  20, r:  0},
+            {x: 570, y: 280, r:  0}
+        ]
     },
-    methods: {
-        /** does A have line of sight to B, excluding circles not_i, not_j? */
-        line_of_sight: function(A, B, not_i, not_j) {
-            for (let k = 0; k < this.circles.length; k++) {
-                if (k != not_i && k != not_j) {
-                    const calculation = segment_circle_intersection(A, B, this.circles[k]);
-                    if (calculation.intersects) {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
+    computed: {
+        edges: function() { return generate_surfing_edges(this.circles); }
     }
 });
