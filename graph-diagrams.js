d3e7b65175cba1560b2205db2e77fd26f6771841
Amit Patel <redblobgames@gmail.com>
2017-04-12 18:39:16 -0700

    Generate hugging edges
    - Eliminate duplicate surfing edges (created when a circle has radius 0)
    - Make a list of nodes from surfing edges (eliminating duplicates)
    - Make a list of hugging edges from nodes
    
    Since this version does not have any of the optimizations described
    later on the page (such as the left/right distinction) there are
    a *lot* of hugging edges even with a small number of circles.

--- graph-diagrams.js
+++ graph-diagrams.js
@@ -16,6 +16,18 @@ function line_of_sight(circles, i, P, j, Q) {
 }
 
 
+/** Return the array with duplicate objects removed,
+    where duplication is by string representation (JSON) 
+ */
+function deduplicate(array) {
+    let map = new Map();
+    for (let x of array) {
+        map.set(JSON.stringify(x), x);
+    }
+    return [...map.values()];
+}
+
+
 /** Generate surfing edges, [{circle: int, x: number, y: number},
                              {circle: int, x: number, y: number}]
  */
@@ -29,23 +41,57 @@ function generate_surfing_edges(circles) {
         edges.push([{circle: i, x: P.x, y: P.y},
                     {circle: j, x: Q.x, y: Q.y}]);
     }
-    
+
+    // some circles have radius 0; they will generate fewer bitangents
     for (let i = 0; i < circles.length; i++) {
         for (let j = 0; j < i; j++) {
             let candidates = [];
             var internal = new InternalBitangents(circles[i], circles[j]);
             add_edge(i, internal.C, j, internal.F);
-            add_edge(i, internal.D, j, internal.E);
+            if (circles[i].r != 0 && circles[j].r != 0) { add_edge(i, internal.D, j, internal.E); }
             var external = new ExternalBitangents(circles[i], circles[j]);
-            add_edge(i, external.C, j, external.F);
-            add_edge(i, external.D, j, external.E);
-            // TODO: handle circles[i].r == 0 or circles[j].r == 0
+            if (circles[i].r != 0 || circles[j].r != 0) { add_edge(i, external.C, j, external.F); }
+            if (circles[i].r != 0 && circles[j].r != 0) { add_edge(i, external.D, j, external.E); }
         }
     }
     return edges;
 }
 
 
+/** Generate nodes from surfing edges */
+function generate_nodes(surfing_edges) {
+    let nodes = [];
+    for (let edge of surfing_edges) {
+        nodes.push(edge[0], edge[1]);
+    }
+    return deduplicate(nodes);
+}
+
+
+/** Generate hugging edges from nodes
+ 
+    Any nodes on the same circle get connected by a hugging edge. 
+*/
+function generate_hugging_edges(nodes) {
+    let buckets = [];
+    for (let node of nodes) {
+        if (buckets[node.circle] === undefined) { buckets[node.circle] = []; }
+        buckets[node.circle].push(node);
+    }
+
+    let hugging_edges = [];
+    for (let bucket of buckets) {
+        for (let i = 0; i < bucket.length; i++) {
+            for (let j = 0; j < i; j++) {
+                hugging_edges.push([bucket[i], bucket[j]]);
+            }
+        }
+    }
+    return hugging_edges;
+}
+
+
+/** Don't allow a circle drag operation if it would touch another circle */
 function no_touching_circle(index) {
     return (x, y) => {
         for (let i = 0; i < this.circles.length; i++) {
@@ -70,7 +116,9 @@ let graph_all_edges = new Vue({
         ]
     },
     computed: {
-        edges: function() { return generate_surfing_edges(this.circles); }
+        surfing_edges: function() { return generate_surfing_edges(this.circles); },
+        nodes: function() { return generate_nodes(this.surfing_edges); },
+        hugging_edges: function() { return generate_hugging_edges(this.nodes); }
     },
     methods: {
         no_touching_circle: no_touching_circle
@@ -93,7 +141,9 @@ let graph_busy_edges = new Vue({
         ]
     },
     computed: {
-        edges: function() { return generate_surfing_edges(this.circles); }
+        surfing_edges: function() { return generate_surfing_edges(this.circles); },
+        nodes: function() { return generate_nodes(this.surfing_edges); },
+        hugging_edges: function() { return generate_hugging_edges(this.nodes); }
     },
     methods: {
         no_touching_circle: no_touching_circle
